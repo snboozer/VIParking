d319e86953c9d6632b6f65e54d6108d6cc036104
Shamera <snboozer@gmail.com>
2019-10-15 20:56:28 -0400

    updated input forms

--- viparking/src/components/SignIn.js
+++ viparking/src/components/SignIn.js
@@ -12,9 +12,6 @@ export const SignIn = () =>(
     <Form.Label>Password</Form.Label>
     <Form.Control type="password" placeholder="Password" />
   </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
   <Button variant="primary" type="submit">
     Submit
   </Button>
--- viparking/src/components/signUpForm.js
+++ viparking/src/components/signUpForm.js
@@ -2,58 +2,55 @@ import React from 'react';
 import { Form, Col, Button } from 'react-bootstrap'
 
 
-export const SignUpForm = () =>(
+export const SignUpForm = () => (
   <Form>
-  <Form.Row>
-    <Form.Group as={Col} controlId="formGridEmail">
-      <Form.Label>Email</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
+    <Form.Row>
+    <Form.Group as={Col} controlId="formGridName">
+      <Form.Label>Name</Form.Label>
+      <Form.Control placeholder="John Smith" />
     </Form.Group>
 
-    <Form.Group as={Col} controlId="formGridPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-  </Form.Row>
+      <Form.Group as={Col} controlId="formGridEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
 
-  <Form.Group controlId="formGridAddress1">
-    <Form.Label>Address</Form.Label>
-    <Form.Control placeholder="1234 Main St" />
-  </Form.Group>
 
-  <Form.Group controlId="formGridAddress2">
-    <Form.Label>Address 2</Form.Label>
-    <Form.Control placeholder="Apartment, studio, or floor" />
-  </Form.Group>
+    </Form.Row>
 
-  <Form.Row>
-    <Form.Group as={Col} controlId="formGridCity">
-      <Form.Label>City</Form.Label>
-      <Form.Control />
-    </Form.Group>
+    <Form.Row>
+      <Form.Group as={Col} controlId="formGridPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
 
-    <Form.Group as={Col} controlId="formGridState">
-      <Form.Label>State</Form.Label>
-      <Form.Control as="select">
-        <option>Choose...</option>
-        <option>...</option>
-      </Form.Control>
+      <Form.Group as={Col} controlId="formGridRePassword">
+        <Form.Label>Re-Enter Password</Form.Label>
+        <Form.Control type="password" placeholder="Re-Enter Password" />
+      </Form.Group>
+    </Form.Row>
+
+    <Form.Group controlId="formGridCarType">
+      <Form.Label>Car Year, Make and Model</Form.Label>
+      <Form.Control placeholder="2006 Nissan Maxima" />
     </Form.Group>
 
-    <Form.Group as={Col} controlId="formGridZip">
-      <Form.Label>Zip</Form.Label>
-      <Form.Control />
-    </Form.Group>
-  </Form.Row>
+    <Form.Row>
+      <Form.Group as={Col} controlId="formGridPlate">
+        <Form.Label>License Plate Number</Form.Label>
+        <Form.Control />
+      </Form.Group>
 
-  <Form.Group id="formGridCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
+      <Form.Group as={Col} controlId="formGridHandicap">
+        <Form.Label>HandiCap#</Form.Label>
+        <Form.Control />
+      </Form.Group>
+    </Form.Row>
 
-  <Button variant="primary" type="submit">
-    Submit
+    <Button variant="primary" type="submit">
+      Submit
   </Button>
-</Form>
+  </Form>
 )
 
 export default SignUpForm;
